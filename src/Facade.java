fd3b1704087b88ad34876b3c350d833abb5d4d3e
Vinay Kantilal Chavhan (Student) <vchavhan@sundevils.asu.edu>
2022-10-18 21:06:09 -0700

    Intial Structure of the code

---
+++ src/Facade.java
@@ -0,0 +1,61 @@
+public class Facade {
+
+	private int UserType; //Buyer: 0, Seller 1 
+
+	private Product theSelectedProduct;
+
+	private int nProductCategory;//0: Meat, 1: Produce. 
+
+	private ClassProductList theProductList;
+
+	private Person thePerson;
+
+	public boolean login() {
+		return false;
+	}
+
+	public void addTrading() {
+
+	}
+
+	public void viewTrading() {
+
+	}
+
+	public void decideBidding() {
+
+	}
+
+	public void discussBidding() {
+
+	}
+
+	public void submitBidding() {
+
+	}
+
+	public void remind() {
+
+	}
+
+	public void createUser() {
+
+	}
+
+	public void createProductList() {
+
+	}
+
+	public void AttachProductToUser() {
+
+	}
+
+	public Product SelectProduct() {
+		return null;
+	}
+
+	public void productOperation() {
+
+	}
+
+}
